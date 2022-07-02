fcde390eedbf2caa51d0228f68c9ca20fa1263da
transmanit <baongan4511@gmail.com>
2022-07-02 16:56:46 +0700

    testing1

--- src/stores/data-demo.js
+++ src/stores/data-demo.js
@@ -1,3 +1,85 @@
 // Rooms
+export const DataRoomsDemo = [
+  {
+    id: 1,
+    nameRoom: "1 Bedroom Luxury Apartment",
+    price: 2443749,
+    quantity: 3,
+    maxPerson: 2,
+    bed: "1 King bed",
+    convenient:
+      "56m²•Pool view•Non-smoking•Kitchen•Wireless Internet•Iron/Ironing board•Air conditioned•Laundry Facilities•Room Safe•Tea/Coffee Maker•Room Service•Daily Room Service•Fridge•DVD Player•Mini Bar•Balcony•Telephone•Bathrobes Provided•Microwave•Television•Hairdryer•Heating•Alarm Clock",
+    introduction:
+      "The popular 56-square-metre (183-square-foot) One Bedroom Apartments have everything you need for the perfect holiday or relaxing retreat – just unpack your suit case and enjoy your days and evenings in your very own suite. Featuring an open plan king bed, living area, fully equipped kitchenette and private balcony, the apartments are designed to provide everything you desire from a first-class resort at a reasonable price and are great for singles, couples or corporate guests.",
+    imageUrl: [
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/476411/476411/1BR_Apartment_.1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477239/477239/1BR_Apartment.2.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477240/477240/1BR_Apartment.3.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477241/477241/1BR_Apartment.4.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477242/477242/1BR_Apartment.5.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/floor_plans/476419/476419/1-bedroom-apartment-1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+    ],
+  },
+  {
+    id: 2,
+    nameRoom: "1 Bedroom Luxury Apartment",
+    price: 2443749,
+    quantity: 3,
+    maxPerson: 2,
+    bed: "1 King bed",
+    convenient:
+      "56m²•Pool view•Non-smoking•Kitchen•Wireless Internet•Iron/Ironing board•Air conditioned•Laundry Facilities•Room Safe•Tea/Coffee Maker•Room Service•Daily Room Service•Fridge•DVD Player•Mini Bar•Balcony•Telephone•Bathrobes Provided•Microwave•Television•Hairdryer•Heating•Alarm Clock",
+    introduction:
+      "The popular 56-square-metre (183-square-foot) One Bedroom Apartments have everything you need for the perfect holiday or relaxing retreat – just unpack your suit case and enjoy your days and evenings in your very own suite. Featuring an open plan king bed, living area, fully equipped kitchenette and private balcony, the apartments are designed to provide everything you desire from a first-class resort at a reasonable price and are great for singles, couples or corporate guests.",
+    imageUrl: [
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/476411/476411/1BR_Apartment_.1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477239/477239/1BR_Apartment.2.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477240/477240/1BR_Apartment.3.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477241/477241/1BR_Apartment.4.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477242/477242/1BR_Apartment.5.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/floor_plans/476419/476419/1-bedroom-apartment-1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+    ],
+  },
+  {
+    id: 3,
+    nameRoom: "1 Bedroom Luxury Apartment",
+    price: 2443749,
+    quantity: 3,
+    maxPerson: 2,
+    bed: "1 King bed",
+    convenient:
+      "56m²•Pool view•Non-smoking•Kitchen•Wireless Internet•Iron/Ironing board•Air conditioned•Laundry Facilities•Room Safe•Tea/Coffee Maker•Room Service•Daily Room Service•Fridge•DVD Player•Mini Bar•Balcony•Telephone•Bathrobes Provided•Microwave•Television•Hairdryer•Heating•Alarm Clock",
+    introduction:
+      "The popular 56-square-metre (183-square-foot) One Bedroom Apartments have everything you need for the perfect holiday or relaxing retreat – just unpack your suit case and enjoy your days and evenings in your very own suite. Featuring an open plan king bed, living area, fully equipped kitchenette and private balcony, the apartments are designed to provide everything you desire from a first-class resort at a reasonable price and are great for singles, couples or corporate guests.",
+    imageUrl: [
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/476411/476411/1BR_Apartment_.1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477239/477239/1BR_Apartment.2.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477240/477240/1BR_Apartment.3.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477241/477241/1BR_Apartment.4.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477242/477242/1BR_Apartment.5.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/floor_plans/476419/476419/1-bedroom-apartment-1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+    ],
+  },
+  {
+    id: 4,
+    nameRoom: "1 Bedroom Luxury Apartment",
+    price: 2443749,
+    quantity: 3,
+    maxPerson: 2,
+    bed: "1 King bed",
+    convenient:
+      "56m²•Pool view•Non-smoking•Kitchen•Wireless Internet•Iron/Ironing board•Air conditioned•Laundry Facilities•Room Safe•Tea/Coffee Maker•Room Service•Daily Room Service•Fridge•DVD Player•Mini Bar•Balcony•Telephone•Bathrobes Provided•Microwave•Television•Hairdryer•Heating•Alarm Clock",
+    introduction:
+      "The popular 56-square-metre (183-square-foot) One Bedroom Apartments have everything you need for the perfect holiday or relaxing retreat – just unpack your suit case and enjoy your days and evenings in your very own suite. Featuring an open plan king bed, living area, fully equipped kitchenette and private balcony, the apartments are designed to provide everything you desire from a first-class resort at a reasonable price and are great for singles, couples or corporate guests.",
+    imageUrl: [
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/476411/476411/1BR_Apartment_.1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477239/477239/1BR_Apartment.2.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477240/477240/1BR_Apartment.3.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477241/477241/1BR_Apartment.4.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/images/477242/477242/1BR_Apartment.5.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+      "https://tbb-prod-apac.imgix.net/attachments/room_type_photos/floor_plans/476419/476419/1-bedroom-apartment-1.jpg?auto=format,compress&fit=crop&crop=entropy&w=300&h=200&q=55",
+    ],
+  },
+];
 // Users list
 // Ordered list
